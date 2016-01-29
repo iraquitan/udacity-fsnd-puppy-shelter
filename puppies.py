caaaf4288cf104daafbcbcea4250156d9f69cd1b
Iraquitan Cordeiro Filho <iraquitanfilho@gmail.com>
2016-01-29 18:13:29 -0300

    feat: Add tables and relations to exercise 3 (one-to-one, many-to-many)

--- puppies.py
+++ puppies.py
@@ -9,12 +9,19 @@
  * Time: 14:16
  * To change this template use File | Settings | File Templates.
 """
-from sqlalchemy import Column, ForeignKey, Integer, String, Date, Numeric
+from sqlalchemy import Column, ForeignKey, Integer, String, Date, Numeric, \
+    Table
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
 
 Base = declarative_base()
+
+adoption_table = Table(
+    'adoption', Base.metadata,
+    Column('adopter_id', Integer, ForeignKey('adopter.id')),
+    Column('puppy_id', Integer, ForeignKey('puppy.id'))
+)
 
 
 class Shelter(Base):
@@ -37,17 +44,33 @@ class Puppy(Base):
     shelter_id = Column(Integer, ForeignKey('shelter.id'))
     shelter = relationship(Shelter)
     weight = Column(Numeric(10))
-    profile = relationship('PuppyProfile', uselist=False, backref='puppy')
+    # profile_id = Column(Integer, ForeignKey('puppy_profile.id'))
+    profile = relationship("PuppyProfile", uselist=False,
+                           back_populates="puppy")
+    adopters = relationship("Adopter", secondary=adoption_table,
+                            back_populates="puppies")
 
 
 # Exercise 3
 class PuppyProfile(Base):
     __tablename__ = 'puppy_profile'
-    # id = Column(Integer, primary_key=True)
-    puppy_id = Column(Integer, ForeignKey('puppy.id'), primary_key=True)
+    id = Column(Integer, primary_key=True)
     picture = Column(String)
     description = Column(String)
     specialNeeds = Column(String)
+    puppy_id = Column(Integer, ForeignKey('puppy.id'))
+    puppy = relationship("Puppy", uselist=False, back_populates="profile")
+
+
+# Exercise 3
+class Adopter(Base):
+    __tablename__ = 'adopter'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    gender = Column(String(6), nullable=False)
+    dateOfBirth = Column(Date)
+    puppies = relationship("Puppy", secondary=adoption_table,
+                           back_populates="adopters")
 
 engine = create_engine('sqlite:///puppyshelter.db')
 
